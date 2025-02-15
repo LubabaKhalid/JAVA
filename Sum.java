32906dca8ffd357a74b42605abe80e15ade94e4a
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-15 17:14:34 +0500

    add image

--- Sum.java
+++ Sum.java
@@ -1,10 +1,14 @@
 import java.util.*;
 public class Sum {
     public static void main(String args[]){
+        //Scanner sc=new Scanner(System.in);
+        //String name=sc.next();
+        //System.out.println(name);
         Scanner sc=new Scanner(System.in);
-        String name=sc.next();
-        System.out.println(name);
-        
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int s=a+b;
+        System.out.print(s);
         
     }
 }
