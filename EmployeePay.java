00e05536a2a3c753f284d15767c6e51815397592
bsdsf22a025 <bsdsf22a025@pucit.edu.pk>
2025-02-17 22:35:58 +0500

    added file

---
+++ EmployeePay.java
@@ -0,0 +1,10 @@
+public class EmployeePay {
+    public static void main(String arg[])
+    {
+        int payAmount=2000;
+        int payPeriods=12;
+        int annualPay;
+        annualPay=payAmount*payPeriods;
+        System.out.println("Total Annual pay of employee is :"+annualPay);
+    }
+}
