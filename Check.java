1d4e1a40e2739072007eaa9339cb7202c5e07d09
lubaba <bsdsf22a025@pucit.edu.pk>
2025-02-28 10:55:17 +0500

    added check.java

---
+++ Check.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Check{
+    public static void main(String arg[]){
+
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        System.out.println("Interger is "+n);
+    }
+}
