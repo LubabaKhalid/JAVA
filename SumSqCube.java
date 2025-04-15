28702d1469f2ef4101750254b6b671ebc8b06a55
lubaba <bsdsf22a025@pucit.edu.pk>
2025-04-15 22:39:27 +0500

    added file

---
+++ SumSqCube.java
@@ -0,0 +1,9 @@
+public class SumSqCube {
+    public static void main(String args[]){
+        int n=10;
+        System.out.println("Digit\tSquare\tCube");
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d\t%d\t%d\n",i,i*i,i*i*i);
+        }
+    }
+}
